e0a4baadf5a3a85d704c205d11564c0dfc3e3c29
Stephen Salt <kntajus@hotmail.com>
2022-09-30 23:08:42 +0100

    Move auto-registration to separate package/repo

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.19
 
 require (
 	github.com/jackc/pgconn v1.13.0
-	github.com/jackc/pgtype v1.12.0
 	github.com/jackc/pgx/v4 v4.17.2
+	github.com/kntajus/pgxtra/v4 v4.0.1
 	github.com/rs/zerolog v1.15.0
 )
 
@@ -15,6 +15,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"context"
 
-	"github.com/jackc/pgtype"
 	"github.com/jackc/pgx/v4"
 	"github.com/kntajus/arraytest/internal/db/store"
+	"github.com/kntajus/pgxtra/v4"
 	"github.com/rs/zerolog/log"
 )
-
-const retrieveCustomEnumArrayTypes = `SELECT arrtype.oid, arrtype.typname
-FROM pg_type arrtype
-JOIN pg_type enumtype ON arrtype.typelem = enumtype.oid AND arrtype.typcategory = 'A'
-WHERE enumtype.typcategory = 'E'`
 
 func main() {
 	conn, err := pgx.Connect(context.Background(), "postgres://arraytest:postgres@arraytest-db:5432/arraytest?sslmode=disable")
@@ -21,7 +16,7 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	err = registerEnumArrayTypes(conn)
+	err = pgxtra.RegisterEnumArrayTypes(conn)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to register custom enum array types")
 	}
@@ -32,33 +27,4 @@ func main() {
 		log.Fatal().Err(err).Send()
 	}
 	log.Info().Msg("Row inserted successfully")
-}
-
-func registerEnumArrayTypes(conn *pgx.Conn) error {
-	enumTypes, err := fetchEnumArrayTypesInfo(conn)
-	if err != nil {
-		return err
-	}
-
-	for k, v := range enumTypes {
-		conn.ConnInfo().RegisterDataType(pgtype.DataType{Value: &pgtype.EnumArray{}, Name: v, OID: k})
-	}
-	return nil
-}
-
-func fetchEnumArrayTypesInfo(conn *pgx.Conn) (map[uint32]string, error) {
-	rows, err := conn.Query(context.Background(), retrieveCustomEnumArrayTypes)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	enumTypes := make(map[uint32]string)
-	for rows.Next() {
-		var oid uint32
-		var name string
-		rows.Scan(&oid, &name)
-		enumTypes[oid] = name
-	}
-	return enumTypes, nil
 }
